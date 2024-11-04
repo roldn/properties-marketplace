d054fd33bd01c063413f82429907257f153ab159
Roldán Francisco <roldanfrancisco.personal@gmail.com>
2024-11-04 02:10:57 -0300

    Test api route creation

---
+++ api/controllers/user.controller.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+export const test =  (req, res) => {
+    res.json({
+        message: "API route is working!",
+    })
+}
--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route';
 
 dotenv.config();
 
@@ -18,3 +19,9 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     
 })
+
+app.get('/test', (req,res) =>{
+    res.send('Hello World!')
+})
+
+app.use("/api/user", userRouter)
