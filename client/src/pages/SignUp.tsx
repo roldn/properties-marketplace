dc061ed6ecc4dc96c2a92615048fda392d1607f0
Roldán Francisco <roldanfrancisco.personal@gmail.com>
2024-11-06 14:08:01 -0300

    sign up page functionality completion

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,33 +1,77 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 type Props = {};
 
 export default function SingUp({}: Props) {
+  const [formData, setFormData] = useState({});
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate()
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault();
+    
+    try {
+      setLoading(true);
+      const res: Response = await fetch('/api/auth/sign-up', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });  
+      const data = await res.json();
+      if(data.success === false){
+        setError(data.message);
+        setLoading(false)
+        return;
+      };
+      setLoading(false)
+      console.log(data);
+    } catch (error:any) {
+      setLoading(false)
+      setError(error.message)
+    }
+    setError(null)
+    navigate('/sign-in')
+  };
+  console.log(formData  )
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form action="" className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading...' : 'Sing Up'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -36,6 +80,7 @@ export default function SingUp({}: Props) {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
